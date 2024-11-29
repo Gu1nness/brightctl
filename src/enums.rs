1287ac53f0ef4eb994c9da72bde07292c22d2d2f
Neha Oudin <neha@oudin.red>
2024-11-29 18:58:46 +0100

    feat: Leave regexes and parse directly

--- src/enums.rs
+++ src/enums.rs
@@ -1,7 +1,5 @@
 use std::{error::Error, str::FromStr};
 
-use regex::Regex;
-
 #[derive(Debug, PartialEq, Eq)]
 pub enum ValueType {
     ABSOLUTE,
@@ -31,25 +29,55 @@ pub enum ValueUpdate {
 impl FromStr for ValueUpdate {
     type Err = Box<dyn Error>;
 
+    //fn from_str(data: &str) -> Result<Self, Self::Err> {
+    //    let re = Regex::new(r"^(?<value>\d+)(?<percent>\%)?(?<sign>[\+\-])?").unwrap();
+    //    let cap_opt = re.captures(data);
+    //    let cap = match &cap_opt {
+    //        None => {
+    //            return Err(format!("Wrong format, expecting {}", re).into());
+    //        }
+    //        Some(data) => data,
+    //    };
+    //    let value = cap["value"].parse()?;
+    //    let sign = cap.name("sign").map(|x| x.as_str());
+    //    match (cap.name("percent"), sign) {
+    //        (None, None) => Ok(Self::Direct(value)),
+    //        (None, Some("+")) => Ok(Self::Delta(value)),
+    //        (None, Some("-")) => Ok(Self::Delta(-value)),
+    //        (Some(_), None) => Ok(Self::Absolute(value)),
+    //        (Some(_), Some("+")) => Ok(Self::Relative(value)),
+    //        (Some(_), Some("-")) => Ok(Self::Relative(-value)),
+    //        (_, _) => panic!("This should be unreachable."),
+    //    }
+    //}
     fn from_str(data: &str) -> Result<Self, Self::Err> {
-        let re = Regex::new(r"^(?<value>\d+)(?<percent>\%?)(?<sign>[\+\-]?)").unwrap();
-        let cap_opt = re.captures(data);
-        let cap = match &cap_opt {
-            None => {
-                return Err(format!("Wrong format, expecting {}", re).into());
+        let chars = data.chars();
+        let mut value: i64 = 0;
+        let mut has_percent = false;
+        let mut has_sign: bool = false;
+        for char in chars {
+            if has_sign {
+                return Err("Invalid format".into());
             }
-            Some(data) => data,
-        };
-        let value = cap["value"].parse()?;
-        let sign = cap.name("sign").map(|x| x.as_str());
-        match (cap["percent"].is_empty(), sign) {
-            (true, Some("")) => Ok(Self::Direct(value)),
-            (true, Some("+")) => Ok(Self::Delta(value)),
-            (true, Some("-")) => Ok(Self::Delta(-value)),
-            (false, Some("")) => Ok(Self::Absolute(value)),
-            (false, Some("+")) => Ok(Self::Relative(value)),
-            (false, Some("-")) => Ok(Self::Relative(-value)),
-            (_, _) => panic!("This should be unreachable."),
+            match (has_percent, char) {
+                (false, x) if x.is_ascii_digit() => {
+                    value = 10 * value + (char.to_digit(10).unwrap() as i64)
+                }
+                (false, '%') => has_percent = true,
+                (_, '+') => has_sign = true,
+                (_, '-') => {
+                    has_sign = true;
+                    value = -value
+                }
+                (_, _) => return Err("Invalid format".into()),
+            }
+        }
+
+        match (has_percent, has_sign) {
+            (false, false) => Ok(Self::Direct(value)),
+            (false, true) => Ok(Self::Delta(value)),
+            (true, false) => Ok(Self::Absolute(value)),
+            (true, true) => Ok(Self::Relative(value)),
         }
     }
 }
@@ -86,5 +114,13 @@ mod tests {
     fn test_parse_50() {
         let val_1 = ValueUpdate::Direct(50);
         assert_eq!(ValueUpdate::from_str("50").unwrap(), val_1);
+    }
+
+    #[test]
+    fn test_invalid_format() {
+        assert!(ValueUpdate::from_str("-50").is_err());
+        assert!(ValueUpdate::from_str("5-0").is_err());
+        assert!(ValueUpdate::from_str("5a0").is_err());
+        assert!(ValueUpdate::from_str("%50").is_err());
     }
 }
